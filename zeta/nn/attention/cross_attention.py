17d6e2fb354f8af2b240e8e1f6e805d887fb44ed
Kye <kye@apacmediasolutions.com>
2023-08-23 14:27:29 -0400

    clean up cross attention

---
+++ zeta/nn/attention/cross_attention.py
@@ -0,0 +1,8 @@
+from zeta.nn.architecture.transformer import AttentionLayers
+
+
+class CrossAttend(AttentionLayers):
+    def __init__(self, **kwargs):
+        super().__init__(cross_attend=True,
+                         only_cross=True,
+                         **kwargs)
