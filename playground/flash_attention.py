e56fefd3ea9658468d33d29bc6a6023c96aa80fa
Kye <kye@apacmediasolutions.com>
2023-08-23 14:37:28 -0400

    playground

---
+++ playground/flash_attention.py
@@ -0,0 +1,11 @@
+import torch
+from zeta import FlashAttention
+
+q = torch.randn(2, 4, 6, 8)
+k = torch.randn(2, 4, 10, 8)
+v = torch.randn(2, 4, 10, 8)
+
+attention = FlashAttention(causal=False, dropout=0.1, flash=False)
+output = attention(q, k, v)
+
+print(output.shape) 
