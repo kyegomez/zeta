c495331bc969aeff94b0df91b95c691fe8cbe42e
Kye <kye@apacmediasolutions.com>
2023-08-22 21:08:05 -0400

    forgot gpt4 import init

--- zeta/__init__.py
+++ zeta/__init__.py
@@ -14,6 +14,12 @@ from zeta.nn.architecture.transformer import (
 from zeta.nn.attention.multiquery_attention import MultiQueryAttention
 from zeta.nn.attention.flash_attention import FlashAttention
 
+
+#models
+from zeta.models.gpt4 import GPT4, GPT4MultiModal
+
+
+#######
 from zeta.nn import *
 from zeta.models import *
 from zeta.training import *
