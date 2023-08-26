bd4e3e75ec4b3e2e411a4d0269c114654916d077
Kye <kye@apacmediasolutions.com>
2023-08-26 11:39:57 -0400

    imports of optims in training

--- zeta/training/__init__.py
+++ zeta/training/__init__.py
@@ -6,4 +6,5 @@ from zeta.training.train import Trainer, train
 
 from zeta.training.optimizers.decoupled_optimizer import decoupled_optimizer
 from zeta.training.optimizers.stable_adam import StableAdamWUnfused
-
+from zeta.training.optimizers.decoupled_lion import DecoupledLionW
+from zeta.training.optimizers.decoupled_sophia import SophiaG
