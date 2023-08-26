bdb614e4319d3cd406ee7db074428af2b6fc89c8
Kye <kye@apacmediasolutions.com>
2023-08-25 23:22:08 -0400

    got-4

--- zeta/models/gpt4.py
+++ zeta/models/gpt4.py
@@ -1,14 +1,12 @@
+import torch
 from torch import nn 
 
 from zeta.nn.architecture.transformer import (
-    AndromedaEmbedding,
     Decoder,
     Encoder,
     Transformer,
     ViTransformerWrapper,
 )
-
-from zeta.nn.architecture.auto_regressive_wrapper import AutoregressiveWrapper
 
 
 class GPT4(nn.Module):
@@ -52,8 +50,8 @@ class GPT4(nn.Module):
                  attn_one_kv_head=True,  # multiquery attention
                  qk_norm=True, 
                  attn_qk_norm=True, 
-                 attn_qk_norm_dim_scale=True, 
-                 embedding_provider=AndromedaEmbedding()):
+                 attn_qk_norm_dim_scale=True,
+                 ):
         super().__init__()
 
         try:
@@ -61,7 +59,6 @@ class GPT4(nn.Module):
                 num_tokens=num_tokens,
                 max_seq_len=max_seq_len,
                 use_abs_pos_emb=use_abs_pos_emb,
-                embedding_provider=embedding_provider,
                 attn_layers=Decoder(
                     dim=dim,
                     depth=depth,
@@ -96,100 +93,61 @@ class GPT4(nn.Module):
 
 
 
-class GPT4MultiModal(nn.Module):
-    """
-    GPT4 is a transformer-based model architecture. It initializes with 
-    a Transformer and AutoregressiveWrapper with default or user-specified parameters.
-
-    Initialize the model with specified or default parameters.
-        Args:
-        - num_tokens: Number of tokens in the vocabulary
-        - max_seq_len: Maximum sequence length
-        - dim: Dimension of the model
-        - depth: Depth of the model
-        - dim_head: Dimension of the model head
-        - heads: Number of heads
-        - use_abs_pos_emb: Whether to use absolute position embedding
-        - alibi_pos_bias: Alibi position bias
-        - alibi_num_heads: Number of alibi heads
-        - rotary_xpos: Rotary position
-        - attn_flash: Attention flash
-        - deepnorm: Deep normalization
-        - shift_tokens: Number of tokens to shift
-        - attn_one_kv_head: Attention one key/value head
-        - qk_norm: Query-key normalization
-        - attn_qk_norm: Attention query-key normalization
-        - attn_qk_norm_dim_scale: Attention query-key normalization dimension scale
-        - embedding_provider: Embedding provider module
-    """
+class GPT4MultiModal(torch.nn.Module):
     def __init__(self, 
-                 num_tokens=50432, 
-                 max_seq_len=8192, 
-                 dim=2560, 
-                 depth=32, 
-                 dim_head=128, 
-                 heads=24,
-                 use_abs_pos_emb=False, 
-                 alibi_pos_bias=True, 
-                 alibi_num_heads=12, 
+                 image_size=256, 
+                 patch_size=32, 
+                 encoder_dim=512, 
+                 encoder_depth=6, 
+                 encoder_heads=8,
+                 num_tokens=20000, 
+                 max_seq_len=1024, 
+                 decoder_dim=512, 
+                 decoder_depth=6, 
+                 decoder_heads=8, 
+                 alibi_num_heads=4,
+                 use_abs_pos_emb=False,
+                 cross_attend=True,
+                 alibi_pos_bias=True,
                  rotary_xpos=True,
-                 attn_flash=True, 
-                 image_size=256,
-                 patch_size=32,
-                 attn_one_kv_head=True, 
-                 qk_norm=True, 
-                 attn_qk_norm=True, 
-                 attn_qk_norm_dim_scale=True, 
-                 embedding_provider=AndromedaEmbedding()):
-        super(GPT4MultiModal).__init__()
-
+                 attn_flash=True,
+                 qk_norm=True):
+        
+        super(GPT4MultiModal, self).__init__()
+        
         self.encoder = ViTransformerWrapper(
             image_size=image_size,
             patch_size=patch_size,
             attn_layers=Encoder(
-                dim=dim,
-                depth=depth,
-                dim_head=dim_head,
-                heads=heads
+                dim=encoder_dim,
+                depth=encoder_depth,
+                heads=encoder_heads
             )
         )
 
-        self.transformer = Transformer(
+        self.decoder = Transformer(
             num_tokens=num_tokens,
             max_seq_len=max_seq_len,
             use_abs_pos_emb=use_abs_pos_emb,
-            embedding_provider=embedding_provider,
             attn_layers=Decoder(
-                dim=dim,
-                depth=depth,
-                dim_head=dim_head,
-                heads=heads,
+                dim=decoder_dim,
+                depth=decoder_depth,
+                heads=decoder_heads,
+                cross_attend=cross_attend,
                 alibi_pos_bias=alibi_pos_bias,
                 alibi_num_heads=alibi_num_heads,
                 rotary_xpos=rotary_xpos,
                 attn_flash=attn_flash,
-                attn_one_kv_head=attn_one_kv_head,
                 qk_norm=qk_norm,
-                attn_qk_norm=attn_qk_norm,
-                attn_qk_norm_dim_scale=attn_qk_norm_dim_scale,
-                cross_attend=True
             )
         )
 
-        self.decoder = AutoregressiveWrapper(self.transformer)
-
-    def forward(self, img, text_tokens, **kwargs):
-        """
-        Forward pass through the model. It expects the input text_tokens.
-        Args:
-        - text_tokens: Input tokens
-        - kwargs: Other arguments
-        Returns:
-        - output from the decoder
-        """
-        try:
-            encoded_img = self.encoder(img, return_embeddings=True)
-            return self.decoder(text_tokens, context=encoded_img)
+    def forward(self, img, text):
+        try:    
+            encoded = self.encoder(img, return_embeddings=True)
+            return self.decoder(text, context=encoded)
         except Exception as error:
             print(f"Failed in forward method: {error}")
             raise
+
+
