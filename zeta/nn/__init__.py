8961b43a5481b49c523efab1d6b03dd1442cc151
Kye <kye@apacmediasolutions.com>
2023-09-05 08:56:53 -0400

    imports

--- zeta/nn/__init__.py
+++ zeta/nn/__init__.py
@@ -1,12 +1,25 @@
 # Copyright (c) 2022 Agora
 # Licensed under The MIT License [see LICENSE for details]
 
+
+# attention
+from zeta.nn.architecture.transformer import Transformer
+from zeta.nn.architecture.local_transformer import LocalTransformer
+from zeta.nn.architecture.parallel_transformer import ParallelTransformerBlock
+from zeta.nn.architecture.auto_regressive_wrapper import AutoregressiveWrapper
+from zeta.nn.architecture.cross_attender import CrossAttender
+
+
+
 ######### Attention
 from zeta.nn.attention.multiquery_attention import MultiQueryAttention
 from zeta.nn import *
 from zeta.nn.attention.cross_attention import CrossAttend
 from zeta.nn.attention.multihead_attention import MultiheadAttention
 from zeta.nn.attention.flash_attention2 import FlashAttentionTwo
+from zeta.nn.attention.local_attention import LocalAttention
+from zeta.nn.attention.local_attention_mha import LocalMHA
+
 
 
 
@@ -32,3 +45,5 @@ from zeta.nn.modules.lora import Lora
 from zeta.nn.modules.feedforward_network import FeedForwardNetwork
 from zeta.nn.modules.droppath import DropPath
 from zeta.nn.modules.token_learner import TokenLearner
+
+
