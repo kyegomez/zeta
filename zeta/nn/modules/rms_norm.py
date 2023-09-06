6cba30e26199a7cecb8f40049f36413ab17c065f
Kye <kye@apacmediasolutions.com>
2023-09-06 15:00:22 -0400

    no cross attend

---
+++ zeta/nn/modules/rms_norm.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class RMSNorm(nn.Module):
+    def __init__(
+        self,
+        dim,
+        groups=1
+    ):
+        super().__init__()
+        self.scale = dim ** -0.5
+        self.gamma = nn.Parameter(torch.ones(groups, dim, 1))
+    
+    def forward(self, x):
+        normed = F.normalize(x, dim=-2)
+        return normed * self.scale * self.gamma
