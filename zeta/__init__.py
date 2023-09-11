f34747609f1a1119e0c9360b099c6704fc73e3a4
Kye <kye@apacmediasolutions.com>
2023-09-11 15:45:39 -0400

    clean up

--- zeta/__init__.py
+++ zeta/__init__.py
@@ -1,46 +1,41 @@
 
 #architecture
+from zeta.models import *
+from zeta.models.andromeda import Andromeda
+
+#models
+from zeta.models.gpt4 import GPT4, GPT4MultiModal
+from zeta.models.palme import PalmE
+
+#######
+from zeta.nn import *
 from zeta.nn.architecture.transformer import (
     AttentionLayers,
     Decoder,
     Encoder,
     Transformer,
 )
-from zeta.nn.architecture.attn_layers import AttentionLayers
-from zeta.nn.architecture.encoder import Encoder
-from zeta.nn.architecture.decoder import Decoder
-
-
-
-#attentions
-from zeta.nn.attention.multiquery_attention import MultiQueryAttention
-from zeta.nn.attention.flash_attention import FlashAttention
 from zeta.nn.attention.dilated_attention import DilatedAttention
+from zeta.nn.attention.flash_attention import FlashAttention
 from zeta.nn.attention.flash_attention2 import FlashAttentionTwo
+
 # from zeta.nn.attention.cross_attention import CrossAttend
 from zeta.nn.attention.multihead_attention import MultiheadAttention
 
+# from zeta.nn.architecture.attn_layers import AttentionLayers
+# from zeta.nn.architecture.encoder import Encoder
+# from zeta.nn.architecture.decoder import Decoder
 
-#models
-from zeta.models.gpt4 import GPT4, GPT4MultiModal
-from zeta.models.andromeda import Andromeda
-from zeta.models.palme import PalmE
-
-
-#######
-from zeta.nn import *
-from zeta.models import *
-from zeta.training import *
-
-
+#attentions
+from zeta.nn.attention.multiquery_attention import MultiQueryAttention
+from zeta.tokenizers.language_tokenizer import LanguageTokenizerGPTX
 
 #tokenizers
 from zeta.tokenizers.multi_modal_tokenizer import MultiModalTokenizer
-from zeta.tokenizers.language_tokenizer import LanguageTokenizerGPTX
-
-#train 
-from zeta.training.train import Trainer, train
-
+from zeta.training import *
 
 #loss
 from zeta.training.loss.nebula import Nebula
+
+#train 
+from zeta.training.train import Trainer, train
--- zeta/nn/attention/__init__.py
+++ zeta/nn/attention/__init__.py
@@ -1,9 +1,10 @@
 """Zeta Halo"""
 
 #attentions
-from zeta.nn.attention.multiquery_attention import MultiQueryAttention
-from zeta.nn.attention.flash_attention import FlashAttention
 from zeta.nn.attention.dilated_attention import DilatedAttention
+from zeta.nn.attention.flash_attention import FlashAttention
 from zeta.nn.attention.flash_attention2 import FlashAttentionTwo
-from zeta.nn.attention.cross_attention import CrossAttend
+
+# from zeta.nn.attention.cross_attention import CrossAttend
 from zeta.nn.attention.multihead_attention import MultiheadAttention
+from zeta.nn.attention.multiquery_attention import MultiQueryAttention
