da3eb6cfdf74665c66eab5f5f593669e1f18872f
Kye <kye@apacmediasolutions.com>
2023-09-23 17:41:55 -0400

    clean up

--- zeta/__init__.py
+++ zeta/__init__.py
@@ -50,8 +50,7 @@ from zeta.nn.modules.dynamic_module import DynamicModule
 from zeta.nn.modules.droppath import DropPath
 from zeta.nn.modules.feedforward_network import FeedForwardNetwork
 from zeta.nn.modules.layernorm import LayerNorm, l2norm
-from zeta.nn.modules.positional_embedding import PositionalEmbedding
-from zeta.nn.modules.relative_positional_embedding import RelativePositionalEmbedding
+
 from zeta.nn.modules.residual import Residual
 from zeta.nn.modules.mlp import MLP
 
