eb20dc4c1939989600af9041c2a3546c50266728
Kye <kye@apacmediasolutions.com>
2023-09-26 20:18:44 -0400

    tiktoken

--- zeta/tokenizers/__init__.py
+++ zeta/tokenizers/__init__.py
@@ -2,3 +2,4 @@ from zeta.tokenizers.language_tokenizer import LanguageTokenizerGPTX
 from zeta.tokenizers.multi_modal_tokenizer import MultiModalTokenizer
 from zeta.tokenizers.sentence_piece import SentencePieceTokenizer
 from zeta.tokenizers.tokenmonster import TokenMonster
+from zeta.tokenizers.tiktoken import TikToken
