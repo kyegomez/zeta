d3fbb2c1dd11e394d28a3e7ec24520e8e6180563
Kye <kye@apacmediasolutions.com>
2023-10-16 21:36:46 -0400

    quick documentation + token monster

--- zeta/quant/quick.py
+++ zeta/quant/quick.py
@@ -49,6 +49,18 @@ class QUIK(nn.Module):
             nn.init.uniform_(self.bias, -bound, bound)
 
     def quantize(self, input_tensor):
+        """
+        Args:
+            input_tensor: input tensor to be quantized
+        
+        Returns:
+            output_tensor: quantized tensor
+            zero_act: zero-point
+            scale_act: scale factor
+        
+        
+
+        """
         zero_act, scale_act = self.find_zero_scale(input_tensor)
         output_tensor = (
             ((input_tensor - zero_act) / scale_act - self.half_range)
@@ -58,6 +70,21 @@ class QUIK(nn.Module):
         return output_tensor, zero_act, scale_act
 
     def dequantize(self, input_tensor, zero_act, scale_act, scale_weight):
+        """
+        Dequantize the input tensor
+
+        Args:
+            input_tensor: input tensor to be dequantized
+            zero_act: zero-point
+            scale_act: scale factor
+            scale_weight: scale factor for weight
+        
+        Returns:
+
+            output_tensor: dequantized tensor
+
+        
+        """
         weights_reduced = self.weight.sum(dim=1)
         x = input_tensor.float() * scale_act * scale_weight
         shift = (zero_act + self.half_range * scale_act) * weights_reduced.unsqueeze(-1)
@@ -65,11 +92,35 @@ class QUIK(nn.Module):
         return output_tensor
 
     def find_zero_scale(self, input_tensor):
+        """
+        Find zero_scale
+
+
+        Args:
+            input_tensor: input tensor to be quantized
+
+        Returns:
+            zero_act: zero-point
+            scale_act: scale factor
+    
+        
+        
+        """
         zero_act = input_tensor.min()
         scale_act = (input_tensor.max() - zero_act) / (2 * self.half_range)
         return zero_act, scale_act
 
     def forward(self, x):
+        """
+        Forward pass of the QUIK layer
+
+        Args:
+            x: input tensor
+
+        Returns:
+            output_tensor: output tensor after forward pass
+        
+        """
         # Quantize activations
         x_quant, zero_act, scale_act = self.quantize(x)
 
@@ -82,3 +133,5 @@ class QUIK(nn.Module):
         # Dequantization
         scale_weight = (self.weight.max() - self.weight.min()) / (2 * self.half_range)
         return self.dequantize(result, zero_act, scale_act, scale_weight)
+
+
