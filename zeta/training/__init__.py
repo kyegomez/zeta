7179641de54c6bb11331e676cb7fbf5a5c31b4f7
Kye <kye@apacmediasolutions.com>
2023-10-22 23:32:40 -0400

    parallel wrapper

--- zeta/training/__init__.py
+++ zeta/training/__init__.py
@@ -3,3 +3,4 @@ from zeta.training.train import Trainer, train
 from zeta.training.dataloader import build_dataloaders, build_pre_tokenized
 from zeta.training.fsdp import fsdp
 from zeta.training.scheduler import get_lr_scheduler_with_warmup
+from zeta.training.parallel_wrapper import ParallelWrapper
