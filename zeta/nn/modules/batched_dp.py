45514bd6686a9e217857a13fb9e230fcb8746280
Kye <kye@apacmediasolutions.com>
2023-10-25 14:58:20 -0400

    docs for token learner, commented out examples

--- zeta/nn/modules/batched_dp.py
+++ zeta/nn/modules/batched_dp.py
@@ -6,6 +6,6 @@ def batched_dot_product(a, b):
     return rearrange(a * b, "b d -> b (d)").sum(dim=-1)
 
 
-x = torch.rand(1, 3)
-model = batched_dot_product(x, x)
-print(model.shape)
+# x = torch.rand(1, 3)
+# model = batched_dot_product(x, x)
+# print(model.shape)
