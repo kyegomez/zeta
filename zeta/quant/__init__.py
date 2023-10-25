d6ee0bae65a6dc77b2299938e5b0c908370e1838
Kye <kye@apacmediasolutions.com>
2023-10-25 18:53:17 -0400

    fixes

--- zeta/quant/__init__.py
+++ zeta/quant/__init__.py
@@ -1,6 +1,7 @@
 from zeta.quant.quick import QUIK
 from zeta.quant.bitlinear import absmax_quantize, BitLinear
 from zeta.quant.ste import STE
+from zeta.quant.qlora import QloraLinear
 
 
 __all__ = ["QUIK", "absmax_quantize", "BitLinear", "STE"]
