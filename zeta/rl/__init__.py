b8b64bfb0a097d18878f174d87125274280608e7
Kye <kye@apacmediasolutions.com>
2023-10-27 14:25:38 -0400

    efficient net, hindsight replay

--- zeta/rl/__init__.py
+++ zeta/rl/__init__.py
@@ -1,5 +1,5 @@
 from zeta.rl.reward_model import RewardModel
 from zeta.rl.actor_critic import ActorCritic, ppo
+from zeta.rl.hindsight_replay import HindsightExperienceReplay
 
-
-__all__ = ["RewardModel", "ActorCritic", "ppo"]
+__all__ = ["RewardModel", "ActorCritic", "ppo", "HindsightExperienceReplay"]
