93cb803477db9bc2311fcd463edc706cf41277fd
Kye <kye@apacmediasolutions.com>
2023-11-23 23:54:21 -0800

    ops work

--- code_quality.sh
+++ code_quality.sh
@@ -5,15 +5,15 @@
 
 # Run autopep8 with max aggressiveness (-aaa) and in-place modification (-i)
 # on all Python files (*.py) under the 'tests' directory.
-autopep8 --in-place --aggressive --aggressive --recursive --experimental --list-fixes tests/
+autopep8 --in-place --aggressive --aggressive --recursive --experimental --list-fixes zeta/
 
 # Run black with default settings, since black does not have an aggressiveness level.
 # Black will format all Python files it finds in the 'tests' directory.
-black --experimental-string-processing tests/
+black --experimental-string-processing zeta/
 
 # Run ruff on the 'tests' directory.
 # Add any additional flags if needed according to your version of ruff.
-ruff tests/ --fix
+ruff zeta/ --fix
 
 # YAPF
 yapf --recursive --in-place --verbose --style=google --parallel tests
