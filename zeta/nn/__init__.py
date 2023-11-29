150f071a85b97bad37d5f23dea49563333f62f14
Kye <kye@apacmediasolutions.com>
2023-11-29 15:34:09 -0800

    init cleaup

--- zeta/nn/__init__.py
+++ zeta/nn/__init__.py
@@ -1,18 +1,16 @@
 # Attention
 # from zeta.nn.attention import *
-from zeta.nn import attention
+from zeta.nn.attention import *
 
-# architecture
-import zeta.structs as architecture
 
 # embeddings
 # from zeta.nn.embeddings import *
-from zeta.nn import embeddings
+from zeta.nn.embeddings import *
 
 # modules
 # from zeta.nn.modules import *
-from zeta.nn import modules
+from zeta.nn.modules import *
 
 # biases
 # from zeta.nn.biases import *
-from zeta.nn import biases
+from zeta.nn.biases import *
