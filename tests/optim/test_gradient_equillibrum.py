[TESTS][FIXUP for Pytests], [FIX][Gradient Ascent, GradientEquillibrum]

tests/optim/test_gradient_equillibrum.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 from torch.optim import SGD
 
-from ge.main import GradientEquilibrum
+from zeta.optim.gradient_equillibrum import GradientEquilibrum
 
 
 # Helper function to create a simple model and loss for testing

tests/structs/test_efficient_net.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 import torch.nn as nn
-from zeta.structs import EfficientNet
+from zeta.structs.efficient_net import EfficientNet
 
 
 @pytest.fixture