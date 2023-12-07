9f9d883b65d5c5726814c5bc3b40ceeb08839ff1
Kye <kye@apacmediasolutions.com>
2023-12-07 12:36:15 -0800

    [PolymorphicNeuronLayer][ from zeta.nn.modules.polymorphic_neuron import PolyMorhphicNeuron E   ImportError: cannot import name PolyMorhphicNeuron from zeta.nn.modules.polymorphic_neuron (/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/zeta/nn/modules/polymorphic_neuron.py)

--- tests/nn/modules/test_polymorphic_neuron.py
+++ tests/nn/modules/test_polymorphic_neuron.py
@@ -2,18 +2,17 @@ import pytest
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from zeta.nn.modules.polymorphic_neuron import PolyMorhphicNeuron
+from zeta.nn.modules.polymorphic_neuron import PolymorphicNeuronLayer
 
-
-# Fixture for creating a sample PolyMorhphicNeuron instance
+# Fixture for creating a sample PolymorphicNeuronLayer instance
 @pytest.fixture
 def sample_neuron():
-    return PolyMorhphicNeuron(in_features=10, out_features=5)
+    return PolymorphicNeuronLayer(in_features=10, out_features=5)
 
 
 # Basic initialization test
 def test_neuron_initialization(sample_neuron):
-    assert isinstance(sample_neuron, PolyMorhphicNeuron)
+    assert isinstance(sample_neuron, PolymorphicNeuronLayer)
     assert sample_neuron.in_features == 10
     assert sample_neuron.out_features == 5
     assert isinstance(sample_neuron.weights, nn.Parameter)
@@ -30,7 +29,7 @@ def test_forward_pass(sample_neuron):
 # Parameterized test for different activation functions
 @pytest.mark.parametrize("activation", [F.relu, F.tanh, F.sigmoid])
 def test_different_activation_functions(activation):
-    neuron = PolyMorhphicNeuron(
+    neuron = PolymorphicNeuronLayer(
         in_features=10, out_features=5, activation_functions=[activation]
     )
     input_tensor = torch.randn(1, 10)
@@ -41,13 +40,13 @@ def test_different_activation_functions(activation):
 # Test for a case where input features and output features are both 0
 def test_zero_features():
     with pytest.raises(ValueError):
-        PolyMorhphicNeuron(in_features=0, out_features=0)
+        PolymorphicNeuronLayer(in_features=0, out_features=0)
 
 
 # Test for a case where the activation functions list is empty
 def test_empty_activation_functions():
     with pytest.raises(ValueError):
-        PolyMorhphicNeuron(
+        PolymorphicNeuronLayer(
             in_features=10, out_features=5, activation_functions=[]
         )
 
@@ -55,7 +54,7 @@ def test_empty_activation_functions():
 # Test for a case where in_features and out_features are negative
 def test_negative_features():
     with pytest.raises(ValueError):
-        PolyMorhphicNeuron(in_features=-10, out_features=-5)
+        PolymorphicNeuronLayer(in_features=-10, out_features=-5)
 
 
 # Test for a case where input tensor shape does not match in_features
@@ -68,14 +67,14 @@ def test_input_tensor_shape_mismatch(sample_neuron):
 # Test for a case where activation functions are not callable
 def test_invalid_activation_functions():
     with pytest.raises(ValueError):
-        PolyMorhphicNeuron(
+        PolymorphicNeuronLayer(
             in_features=10, out_features=5, activation_functions=[1, 2, 3]
         )
 
 
 # Test for a case where the forward pass is called without initializing weights and bias
 def test_forward_pass_without_initialization():
-    neuron = PolyMorhphicNeuron(in_features=10, out_features=5)
+    neuron = PolymorphicNeuronLayer(in_features=10, out_features=5)
     input_tensor = torch.randn(1, 10)
     with pytest.raises(RuntimeError):
         neuron(input_tensor)
