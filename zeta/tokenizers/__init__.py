8a29d971efcd2174bef9ac3aa4681f8db8c184aa
Kye <kye@apacmediasolutions.com>
2023-12-11 23:10:11 -0800

    [LLamaTokenizer]

--- zeta/tokenizers/__init__.py
+++ zeta/tokenizers/__init__.py
@@ -2,7 +2,7 @@ from zeta.tokenizers.language_tokenizer import LanguageTokenizerGPTX
 from zeta.tokenizers.multi_modal_tokenizer import MultiModalTokenizer
 from zeta.tokenizers.sentence_piece import SentencePieceTokenizer
 from zeta.tokenizers.tokenmonster import TokenMonster
-
+from zeta.tokenizers.llama_sentencepiece import LLamaTokenizer
 # from zeta.tokenizers.tiktoken import TikToken
 
 __all__ = [
@@ -10,5 +10,6 @@ __all__ = [
     "MultiModalTokenizer",
     "SentencePieceTokenizer",
     "TokenMonster",
+    "LLamaTokenizer",
     # "TikToken",
 ]
