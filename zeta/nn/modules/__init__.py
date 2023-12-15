db8de360773919ab1745ce4db07dd6a00a7d2896
Kye <kye@apacmediasolutions.com>
2023-12-15 00:04:53 -0800

    [SwiGLU]

--- zeta/nn/modules/__init__.py
+++ zeta/nn/modules/__init__.py
@@ -1,53 +1,53 @@
+from zeta.nn.modules.adaptive_conv import AdaptiveConv3DMod
+from zeta.nn.modules.adaptive_layernorm import AdaptiveLayerNorm
 from zeta.nn.modules.cnn_text import CNNNew
 from zeta.nn.modules.combined_linear import CombinedLinear
 from zeta.nn.modules.convnet import ConvNet
 from zeta.nn.modules.droppath import DropPath
 from zeta.nn.modules.dynamic_module import DynamicModule
+from zeta.nn.modules.ether import Ether
 from zeta.nn.modules.exo import Exo
 from zeta.nn.modules.fast_text import FastTextNew
+from zeta.nn.modules.feedforward import FeedForward
 from zeta.nn.modules.feedforward_network import FeedForwardNetwork
+from zeta.nn.modules.flexible_mlp import CustomMLP
+from zeta.nn.modules.fractorial_net import FractalBlock, FractalNetwork
+from zeta.nn.modules.h3 import H3Layer
+from zeta.nn.modules.itca import IterativeCrossSelfAttention
+from zeta.nn.modules.lang_conv_module import ConvolutionLanguageBlock
 from zeta.nn.modules.layernorm import LayerNorm, l2norm
+from zeta.nn.modules.leaky_relu import LeakyRELU
+from zeta.nn.modules.log_ff import LogFF, compute_entropy_safe
 from zeta.nn.modules.lora import Lora
 from zeta.nn.modules.mbconv import MBConv
 from zeta.nn.modules.mlp import MLP
+from zeta.nn.modules.mlp_mixer import MLPMixer
+from zeta.nn.modules.nebula import Nebula
+from zeta.nn.modules.polymorphic_activation import PolymorphicActivation
+from zeta.nn.modules.polymorphic_neuron import PolymorphicNeuronLayer
+from zeta.nn.modules.prenorm import PreNorm
 from zeta.nn.modules.pulsar import Pulsar
 from zeta.nn.modules.residual import Residual
 from zeta.nn.modules.resnet import ResNet
 from zeta.nn.modules.rms_norm import RMSNorm
 from zeta.nn.modules.rnn_nlp import RNNL
+from zeta.nn.modules.s4 import s4d_kernel
 from zeta.nn.modules.shufflenet import ShuffleNet
+from zeta.nn.modules.sig_lip import SigLipLoss
 from zeta.nn.modules.simple_attention import simple_attention
+from zeta.nn.modules.simple_feedforward import SimpleFeedForward
+from zeta.nn.modules.simple_res_block import SimpleResBlock
+from zeta.nn.modules.skipconnection import SkipConnection
 from zeta.nn.modules.spacial_transformer import SpacialTransformer
 from zeta.nn.modules.subln import SubLN
 from zeta.nn.modules.super_resolution import SuperResolutionNet
-from zeta.nn.modules.token_learner import TokenLearner
-from zeta.nn.modules.yolo import yolo
-from zeta.nn.modules.ether import Ether
-from zeta.nn.modules.nebula import Nebula
-from zeta.nn.modules.adaptive_conv import AdaptiveConv3DMod
 from zeta.nn.modules.time_up_sample import TimeUpSample2x
-from zeta.nn.modules.video_autoencoder import CausalConv3d
-from zeta.nn.modules.simple_res_block import SimpleResBlock
-from zeta.nn.modules.sig_lip import SigLipLoss
-from zeta.nn.modules.simple_feedforward import SimpleFeedForward
+from zeta.nn.modules.token_learner import TokenLearner
 from zeta.nn.modules.unet import Unet
+from zeta.nn.modules.video_autoencoder import CausalConv3d
 from zeta.nn.modules.visual_expert import VisualExpert
-from zeta.nn.modules.feedforward import FeedForward
-from zeta.nn.modules.skipconnection import SkipConnection
-from zeta.nn.modules.log_ff import LogFF, compute_entropy_safe
-from zeta.nn.modules.polymorphic_neuron import PolymorphicNeuronLayer
-from zeta.nn.modules.flexible_mlp import CustomMLP
-from zeta.nn.modules.fractorial_net import FractalBlock, FractalNetwork
-from zeta.nn.modules.polymorphic_activation import PolymorphicActivation
-from zeta.nn.modules.prenorm import PreNorm
-from zeta.nn.modules.itca import IterativeCrossSelfAttention
-from zeta.nn.modules.lang_conv_module import ConvolutionLanguageBlock
-from zeta.nn.modules.s4 import s4d_kernel
-from zeta.nn.modules.h3 import H3Layer
-from zeta.nn.modules.mlp_mixer import MLPMixer
-from zeta.nn.modules.leaky_relu import LeakyRELU
-from zeta.nn.modules.adaptive_layernorm import AdaptiveLayerNorm
-
+from zeta.nn.modules.yolo import yolo
+from zeta.nn.modules.swiglu import SwiGLU, SwiGLUStacked
 
 # from zeta.nn.modules.img_reshape import image_reshape
 # from zeta.nn.modules.flatten_features import flatten_features
@@ -111,4 +111,6 @@ __all__ = [
     "MLPMixer",
     "LeakyRELU",
     "AdaptiveLayerNorm",
+    "SwiGLU",
+    "SwiGLUStacked",
 ]
