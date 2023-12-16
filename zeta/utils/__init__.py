9e6bfeb920a89632576bd26528067695997e0a1f
Kye <kye@apacmediasolutions.com>
2023-12-16 18:07:16 -0500

    [zeta.utils][__init__][CLEANUP]

--- zeta/utils/__init__.py
+++ zeta/utils/__init__.py
@@ -1,3 +1,17 @@
 # Copyright (c) 2022 Agora
 # Licensed under The MIT License [see LICENSE for details]
-from zeta.utils.main import *
+from zeta.utils.cuda_memory_wrapper import track_cuda_memory_usage
+
+from zeta.utils.benchmark import (
+    benchmark,
+    print_cuda_memory_usage,
+    save_memory_snapshot,
+)
+
+
+__all__ = [
+    "track_cuda_memory_usage",
+    "benchmark",
+    "print_cuda_memory_usage",
+    "save_memory_snapshot",
+]
