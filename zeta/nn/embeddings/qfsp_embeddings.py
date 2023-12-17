744b8603c3a7ca54f9756179d8808dae85a1d218
Kye <kye@apacmediasolutions.com>
2023-12-16 23:51:56 -0500

    [CLEANUP]

--- zeta/nn/embeddings/qfsp_embeddings.py
+++ zeta/nn/embeddings/qfsp_embeddings.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class QuantumSuperpositionEmbeddings(nn.Module):
     """
     QuantumSuperpositionEmbeddings with multiple collapse mechanisms.
@@ -17,28 +18,39 @@ class QuantumSuperpositionEmbeddings(nn.Module):
         self.superposed_embeddings = nn.Embedding(vocab_size, embed_dim)
         self.linear_transform = nn.Linear(2 * embed_dim, embed_dim)
 
-    def forward(self, input_ids, context_vector, collapse_mode='weighted_sum'):
+    def forward(self, input_ids, context_vector, collapse_mode="weighted_sum"):
         base_embeds = self.base_embeddings(input_ids)
         superposed_embeds = self.superposed_embeddings(input_ids)
 
-        if collapse_mode == 'weighted_sum':
-            collapsed_embeds = base_embeds + context_vector.unsqueeze(-1) * superposed_embeds
-        elif collapse_mode == 'dot_product':
-            scale = torch.sum(superposed_embeds * context_vector.unsqueeze(-1), dim=-1, keepdim=True)
+        if collapse_mode == "weighted_sum":
+            collapsed_embeds = (
+                base_embeds + context_vector.unsqueeze(-1) * superposed_embeds
+            )
+        elif collapse_mode == "dot_product":
+            scale = torch.sum(
+                superposed_embeds * context_vector.unsqueeze(-1),
+                dim=-1,
+                keepdim=True,
+            )
             collapsed_embeds = base_embeds + scale * superposed_embeds
-        elif collapse_mode == 'cosine_similarity':
-            scale = F.cosine_similarity(superposed_embeds, context_vector.unsqueeze(-1), dim=-1).unsqueeze(-1)
+        elif collapse_mode == "cosine_similarity":
+            scale = F.cosine_similarity(
+                superposed_embeds, context_vector.unsqueeze(-1), dim=-1
+            ).unsqueeze(-1)
             collapsed_embeds = base_embeds + scale * superposed_embeds
-        elif collapse_mode == 'gated':
+        elif collapse_mode == "gated":
             gate = torch.sigmoid(context_vector)
-            collapsed_embeds = base_embeds + gate.unsqueeze(-1) * superposed_embeds
-        elif collapse_mode == 'concat_linear':
+            collapsed_embeds = (
+                base_embeds + gate.unsqueeze(-1) * superposed_embeds
+            )
+        elif collapse_mode == "concat_linear":
             concatenated = torch.cat([base_embeds, superposed_embeds], dim=-1)
             collapsed_embeds = self.linear_transform(concatenated)
         else:
             raise ValueError("Invalid collapse mode selected")
 
         return collapsed_embeds
+
 
 # # Example Usage
 # vocab_size = 10000
