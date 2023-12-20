6ce1f0f6a50937ef1fd75dc2af0f1e0505074092
Kye <kye@apacmediasolutions.com>
2023-12-20 12:55:34 -0500

    [FEATS] [FusedDropoutLayerNorm] [FusedDenseGELUDense]

--- tests/nn/modules/test_fused_gelu_dense.py
+++ tests/nn/modules/test_fused_gelu_dense.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 from zeta.nn.modules.fused_gelu_dense import FusedDenseGELUDense
 
+
 def test_class_init():
     model = FusedDenseGELUDense(512, 1024)
 
@@ -11,14 +12,18 @@ def test_class_init():
     assert model.has_fp16_weights == False
     assert model.threshold == 6.0
 
+
 def test_class_init_with_args():
-    model = FusedDenseGELUDense(512, 1024, bias=False, has_fp16_weights=True, threshold=5.0)
+    model = FusedDenseGELUDense(
+        512, 1024, bias=False, has_fp16_weights=True, threshold=5.0
+    )
 
     assert model.dim == 512
     assert model.dim_out == 1024
     assert model.bias == False
     assert model.has_fp16_weights == True
     assert model.threshold == 5.0
+
 
 def test_forward():
     model = FusedDenseGELUDense(512, 1024)
@@ -27,12 +32,14 @@ def test_forward():
 
     assert out.shape == torch.Size([1, 512])
 
+
 def test_forward_with_different_input():
     model = FusedDenseGELUDense(512, 1024)
     x = torch.randn(2, 512)
     out = model(x)
 
     assert out.shape == torch.Size([2, 512])
+
 
 def test_forward_with_different_dim():
     model = FusedDenseGELUDense(256, 512)
@@ -41,12 +48,14 @@ def test_forward_with_different_dim():
 
     assert out.shape == torch.Size([1, 256])
 
+
 def test_forward_with_different_dim_out():
     model = FusedDenseGELUDense(512, 2048)
     x = torch.randn(1, 512)
     out = model(x)
 
     assert out.shape == torch.Size([1, 512])
+
 
 def test_forward_with_no_bias():
     model = FusedDenseGELUDense(512, 1024, bias=False)
@@ -55,12 +64,14 @@ def test_forward_with_no_bias():
 
     assert out.shape == torch.Size([1, 512])
 
+
 def test_forward_with_fp16_weights():
     model = FusedDenseGELUDense(512, 1024, has_fp16_weights=True)
     x = torch.randn(1, 512)
     out = model(x)
 
     assert out.shape == torch.Size([1, 512])
+
 
 def test_forward_with_different_threshold():
     model = FusedDenseGELUDense(512, 1024, threshold=5.0)
