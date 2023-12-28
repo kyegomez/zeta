1dc8a078450fc7dd6504cded647424513c4c8f31
vyomakesh09 <vyomkeshkesh@gmail.com>
2023-12-28 01:23:03 +0000

    modified:   tests/models/test_navit.py modified:   tests/models/test_vit.py modified:   tests/nn/modules/test_linearactivation.py modified:   tests/structs/test_localtransformer.py modified:   tests/structs/test_transformer.py modified:   zeta/nn/modules/test_dense_connect.py

--- tests/nn/modules/test_linearactivation.py
+++ tests/nn/modules/test_linearactivation.py
@@ -13,14 +13,14 @@ def test_LinearActivation_init():
     "input_tensor", [(torch.tensor([1, 2, 3])), (torch.tensor([-1, 0, 1]))]
 )
 def test_LinearActivation_forward(input_tensor):
-    """Test if the forward method of LinearActivation class retruns the same input tensor."""
+    """Test if the forward method of LinearActivation class returns the same input tensor."""
     act = LinearActivation()
     assert torch.equal(act.forward(input_tensor), input_tensor)
 
 
-@pytest.mark.parametrize("input_tensor", [(torch.tensor([1, 2, "a"]))])
-def test_LinearActivation_forward_error(input_tensor):
+def test_LinearActivation_forward_error():
     """Test if the forward method of LinearActivation class raises an error when input tensor is not valid."""
     act = LinearActivation()
     with pytest.raises(TypeError):
-        act.forward(input_tensor)
+        invalid_input = [1, 2, "a"]
+        act.forward(torch.tensor(invalid_input))
--- zeta/nn/modules/test_dense_connect.py
+++ zeta/nn/modules/test_dense_connect.py
@@ -1,40 +1,36 @@
 import torch
 import torch.nn as nn
-import unittest
-
+import pytest
 from zeta.nn.modules.dense_connect import DenseBlock
 
 
-class DenseBlockTestCase(unittest.TestCase):
-    def setUp(self):
-        self.submodule = nn.Linear(10, 5)
-        self.dense_block = DenseBlock(self.submodule)
-
-    def test_forward(self):
-        x = torch.randn(32, 10)
-        output = self.dense_block(x)
-
-        self.assertEqual(output.shape, (32, 15))  # Check output shape
-        self.assertTrue(
-            torch.allclose(output[:, :10], x)
-        )  # Check if input is preserved
-        self.assertTrue(
-            torch.allclose(output[:, 10:], self.submodule(x))
-        )  # Check submodule output
-
-    def test_initialization(self):
-        self.assertEqual(
-            self.dense_block.submodule, self.submodule
-        )  # Check submodule assignment
-
-    def test_docstrings(self):
-        self.assertIsNotNone(
-            DenseBlock.__init__.__doc__
-        )  # Check if __init__ has a docstring
-        self.assertIsNotNone(
-            DenseBlock.forward.__doc__
-        )  # Check if forward has a docstring
+@pytest.fixture
+def dense_block():
+    submodule = nn.Linear(10, 5)
+    return DenseBlock(submodule)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_forward(dense_block):
+    x = torch.randn(32, 10)
+    output = dense_block(x)
+
+    assert output.shape == (32, 15)  # Check output shape
+    assert torch.allclose(output[:, :10], x)  # Check if input is preserved
+    assert torch.allclose(
+        output[:, 10:], dense_block.submodule(x)
+    )  # Check submodule output
+
+
+def test_initialization(dense_block):
+    assert isinstance(dense_block.submodule, nn.Linear)  # Check submodule type
+    assert dense_block.submodule.in_features == 10  # Check input features
+    assert dense_block.submodule.out_features == 5  # Check output features
+
+
+def test_docstrings():
+    assert (
+        DenseBlock.__init__.__doc__ is not None
+    )  # Check if __init__ has a docstring
+    assert (
+        DenseBlock.forward.__doc__ is not None
+    )  # Check if forward has a docstring
