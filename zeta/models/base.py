7d68eb7d12008ac82d9f3a5b498d65bdca3fb31d
evelynmitchell <efmphone@gmail.com>
2023-12-29 10:37:13 -0700

    raise NotImplemente in base.py

--- zeta/models/base.py
+++ zeta/models/base.py
@@ -3,7 +3,7 @@ from abc import ABC
 
 class BaseModel(ABC):
     def __init__(self, *args, **kwargs):
-        pass
+        raise NotImplementedError
 
     def forward(self):
-        pass
+        raise NotImplementedError
