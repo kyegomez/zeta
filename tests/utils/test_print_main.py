6330be09549f221bf5a60333d7e3cffaea8f581e
evelynmitchell <efmphone@gmail.com>
2023-12-29 19:25:55 -0700

    not stderr

--- tests/utils/test_print_main.py
+++ tests/utils/test_print_main.py
@@ -11,11 +11,11 @@ def message():
 
 
 # Basic Test
-def test_print_main_without_dist(message, capsys):
+def test_print_main_without_dist(message):
     """Test print_main without distribution"""
     print_main(message)
-    captured = capsys.readouterr()
-    assert captured == "This is the test message!" + "\n"
+    captured = capsys.readout()
+    assert captured.out  == message + "\n"
 
 
 # Utilizing Mocks and Parameterized Testing
