58eb9501d73b8b1c533383b622ba5b6693566230
Kye <kye@apacmediasolutions.com>
2023-12-30 17:41:57 -0500

    [FEAT][VisionEncoder]

---
+++ tests/nn/modules/test_adaptive_rmsnorm.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+from zeta.nn.modules.adaptive_rmsnorm import AdaptiveRMSNorm
+
+
+def test_adaptive_rmsnorm_init():
+    arn = AdaptiveRMSNorm(10, dim_cond=5)
+    assert isinstance(arn, AdaptiveRMSNorm)
+    assert arn.dim_cond == 5
+    assert arn.channel_first == False
+    assert arn.scale == 10**0.5
+    assert isinstance(arn.to_gamma, nn.Linear)
+    assert arn.to_bias is None
+
+
+def test_adaptive_rmsnorm_init_with_bias():
+    arn = AdaptiveRMSNorm(10, dim_cond=5, bias=True)
+    assert isinstance(arn.to_bias, nn.Linear)
+
+
+def test_adaptive_rmsnorm_forward():
+    arn = AdaptiveRMSNorm(10, dim_cond=5)
+    x = torch.randn(2, 10)
+    cond = torch.randn(2, 5)
+    output = arn.forward(x, cond=cond)
+    assert output.shape == (2, 10)
+
+
+def test_adaptive_rmsnorm_forward_with_bias():
+    arn = AdaptiveRMSNorm(10, dim_cond=5, bias=True)
+    x = torch.randn(2, 10)
+    cond = torch.randn(2, 5)
+    output = arn.forward(x, cond=cond)
+    assert output.shape == (2, 10)
+
+
+def test_adaptive_rmsnorm_forward_channel_first():
+    arn = AdaptiveRMSNorm(10, dim_cond=5, channel_first=True)
+    x = torch.randn(2, 10, 3, 3)
+    cond = torch.randn(2, 5)
+    output = arn.forward(x, cond=cond)
+    assert output.shape == (2, 10, 3, 3)
---
+++ tests/structs/test_simple_vision_encoder.py
@@ -0,0 +1,27 @@
+import torch
+from zeta.structs.simple_vision_encoder import SimpleVisionEncoder
+
+
+def test_simple_vision_encoder_init():
+    sve = SimpleVisionEncoder()
+    assert sve.size == (384, 384)
+    assert sve.model_name == "vikhyatk/moondream0"
+    assert sve.return_shape == False
+    assert isinstance(sve.model, torch.jit.ScriptModule)
+    assert sve.preprocess.transforms[-1].scale == True
+    assert sve.preprocess.transforms[-1].dtype == torch.float32
+
+
+def test_simple_vision_encoder_init_custom_size():
+    sve = SimpleVisionEncoder(size=(512, 512))
+    assert sve.size == (512, 512)
+
+
+def test_simple_vision_encoder_init_custom_model_name():
+    sve = SimpleVisionEncoder(model_name="custom/model")
+    assert sve.model_name == "custom/model"
+
+
+def test_simple_vision_encoder_init_return_shape():
+    sve = SimpleVisionEncoder(return_shape=True)
+    assert sve.return_shape == True
