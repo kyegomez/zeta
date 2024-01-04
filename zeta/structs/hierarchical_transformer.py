4f48770624a7433798cbe537f421eeb20a07c5b2
Kye <kye@apacmediasolutions.com>
2024-01-03 23:29:47 -0500

    [FEAT][MambaBlock] [Mamba]

--- zeta/structs/hierarchical_transformer.py
+++ zeta/structs/hierarchical_transformer.py
@@ -713,7 +713,7 @@ class HierarchicalTransformer(nn.Module):
         self, prompt, seq_len, temperature=1.0, filter_thres=0.9, **kwargs
     ):
         # einops conflicts with ruff, so noqa on next line
-        b, t, device = *prompt.shape, prompt.device # noqa: F841
+        b, t, device = *prompt.shape, prompt.device  # noqa: F841
 
         out = prompt
 
