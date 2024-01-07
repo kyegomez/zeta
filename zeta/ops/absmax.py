faec1ea798ae141e9851ff2bcc1e1ab83eb9b441
Kye <kye@apacmediasolutions.com>
2024-01-07 02:15:55 -0500

    [FEAT][absmax]'

---
+++ zeta/ops/absmax.py
@@ -0,0 +1,15 @@
+import torch 
+from torch import Tensor
+
+def absmax(x: Tensor):
+    """
+    Compute the absolute maximum value of a tensor.
+
+    Args:
+        x (Tensor): The input tensor.
+
+    Returns:
+        Tensor: The absolute maximum value of the tensor.
+    """
+    return torch.max(torch.abs(x))
+
