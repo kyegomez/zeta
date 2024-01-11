d75ac2ab5a9915264325b1f6e89ce6f8e600ed18
Kye <kye@apacmediasolutions.com>
2024-01-11 00:09:03 -0500

    [1.7.0]

--- zeta/nn/modules/patch_img.py
+++ zeta/nn/modules/patch_img.py
@@ -1,11 +1,8 @@
-import torch 
-from torch import nn, Tensor, einsum
+from torch import Tensor
 from einops import rearrange
 
+
 def patch_img(x: Tensor, patches: int):
-    return rearrange(x, "b c (h p1) (w p2) -> b (h w) (p1 p2 c)", p1=patches, p2=patches)
-    
-    
-# x = torch.randn(2, 3, 32, 32)
-# x = patch_img(x, 4)
-# print(x.shape)
+    return rearrange(
+        x, "b c (h p1) (w p2) -> b (h w) (p1 p2 c)", p1=patches, p2=patches
+    )
