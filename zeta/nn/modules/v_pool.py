e0c26db8f0fe6e78c4c436918e2c04d7d0138f7a
Kye <kye@apacmediasolutions.com>
2024-01-12 11:29:51 -0500

    [FEATS][to_patch_embedding] [posemb_sincos_2d] [VisionAttention] [VitTransformerBlock] [VLayerNorm][ [Parallel] [DepthWiseConv2d] [Pool]

--- zeta/nn/modules/v_pool.py
+++ zeta/nn/modules/v_pool.py
@@ -1,19 +1,33 @@
-import torch 
+import torch
 from torch import nn, Tensor
 from einops import rearrange
 from math import sqrt
 
+
 class DepthWiseConv2d(nn.Module):
-    def __init__(self, dim_in, dim_out, kernel_size, padding, stride, bias = True):
+    def __init__(
+        self, dim_in, dim_out, kernel_size, padding, stride, bias=True
+    ):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(dim_in, dim_out, kernel_size = kernel_size, padding = padding, groups = dim_in, stride = stride, bias = bias),
-            nn.Conv2d(dim_out, dim_out, kernel_size = 1, bias = bias)
+            nn.Conv2d(
+                dim_in,
+                dim_out,
+                kernel_size=kernel_size,
+                padding=padding,
+                groups=dim_in,
+                stride=stride,
+                bias=bias,
+            ),
+            nn.Conv2d(dim_out, dim_out, kernel_size=1, bias=bias),
         )
+
     def forward(self, x):
         return self.net(x)
 
+
 # pooling layer
+
 
 class Pool(nn.Module):
     def __init__(self, dim: int):
@@ -25,9 +39,11 @@ class Pool(nn.Module):
 
         """
         super().__init__()
-        self.downsample = DepthWiseConv2d(dim, dim * 2, kernel_size=3, stride=2, padding=1)
+        self.downsample = DepthWiseConv2d(
+            dim, dim * 2, kernel_size=3, stride=2, padding=1
+        )
         self.cls_ff = nn.Linear(dim, dim * 2)
-        
+
     def forward(self, x: Tensor):
         """
         Forward pass of the Pool module.
@@ -41,7 +57,9 @@ class Pool(nn.Module):
         """
         cls_token, tokens = x[:, :1], x[:, 1:]
         cls_token = self.cls_ff(cls_token)
-        tokens = rearrange(tokens, "b (h w) c -> b c h w", h=int(sqrt(tokens.shape[1])))
+        tokens = rearrange(
+            tokens, "b (h w) c -> b c h w", h=int(sqrt(tokens.shape[1]))
+        )
         tokens = self.downsample(tokens)
         tokens = rearrange(tokens, "b c h w -> b (h w) c")
         return torch.cat((cls_token, tokens), dim=1)
