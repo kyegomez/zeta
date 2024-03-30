f0dbbac434118bb4ad6ab06553009e6afed29489
simudt <simuniliarc@gmail.com>
2024-03-30 12:50:53 +0300

    [INIT] progress on act kernels

--- zeta/experimental/triton/activations/functions.py
+++ zeta/experimental/triton/activations/functions.py
@@ -11,7 +11,6 @@ class Functions:
     def tanh_activation_kernel(
         x_ptr,
         out_ptr,
-        axis_ld,
         n_elements: int,
         BLOCK_SIZE: tl.constexpr,
     ):
@@ -22,8 +21,8 @@ class Functions:
         x = tl.load(x_ptr + offsets, mask=mask)
 
         exp2x = tl.exp(2 * x)
-        tanh_x = 1 - 2 / (exp2x + 1)
-        tl.store(out_ptr + offsets, tanh_x, mask=mask)
+        output = 1 - 2 / (exp2x + 1)
+        tl.store(out_ptr + offsets, output, mask=mask)
 
     @staticmethod
     @triton.jit
@@ -38,4 +37,46 @@ class Functions:
 
         shape_condition = tl.where(x < -1, -1, x)
         output = tl.where(x > 1, 1, shape_condition)
+        tl.store(output_ptr + offsets, output, mask=mask)
+
+    @staticmethod
+    @triton.jit
+    def relu_activation_kernel(
+        x_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr
+    ):
+        idx = tl.program_id(0)
+        block_st = idx * BLOCK_SIZE
+        offsets = block_st + tl.arange(0, BLOCK_SIZE)
+        mask = offsets < n_elements
+        x = tl.load(x_ptr + offsets, mask=mask)
+
+        output = tl.maximum(0, x)
+        tl.store(output_ptr + offsets, output, mask=mask)
+
+    @staticmethod
+    @triton.jit
+    def relu6_activation_kernel(
+        x_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr
+    ):
+        idx = tl.program_id(0)
+        block_st = idx * BLOCK_SIZE
+        offsets = block_st + tl.arange(0, BLOCK_SIZE)
+        mask = offsets < n_elements
+        x = tl.load(x_ptr + offsets, mask=mask)
+
+        output = tl.minimum(tl.maximum(x, 0), 6.0)
+        tl.store(output_ptr + offsets, output, mask=mask)
+
+    @staticmethod
+    @triton.jit
+    def leaky_relu_activation_kernel(
+        x_ptr, output_ptr, n_elements, alpha, BLOCK_SIZE: tl.constexpr
+    ):
+        idx = tl.program_id(0)
+        block_st = idx * BLOCK_SIZE
+        offsets = block_st + tl.arange(0, BLOCK_SIZE)
+        mask = offsets < n_elements
+        x = tl.load(x_ptr + offsets, mask=mask)
+
+        output = tl.maximum(x, alpha * x)
         tl.store(output_ptr + offsets, output, mask=mask)
